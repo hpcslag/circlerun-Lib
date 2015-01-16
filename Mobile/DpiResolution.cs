c7d5735b4e4ad1d55049eb19cbe90f4788976475
gsp40213 <gsp40213@gmail.com>
2015-01-16 18:00:54 +0800

    Mobile Screen Feature

---
+++ Mobile/DpiResolution.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DpiResolution {
+
+	float width = Screen.width / 1280 / 2;
+	float height = Screen.height / 720 / 2;
+
+	public float setScreenWidth(float setWidth){
+
+		setWidth += width;
+
+		return setWidth;
+	}
+
+	public float setScreenHeight(float setHeight){
+
+		setHeight += height;
+
+		return setHeight;
+	}
+
+	public float getScreenWidth(){
+		return Screen.width;
+	}
+	public float getScreenHeight(){
+		return Screen.height;
+	}
+}
